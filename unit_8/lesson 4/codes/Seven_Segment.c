5a99da49954a56f35361138a664f75be3b8c830a
adem <adem.marangoz95@gmail.com>
2022-08-30 21:25:38 -0700

    uint8 lesson 4

---
+++ unit_8/lesson 4/codes/Seven_Segment.c
@@ -0,0 +1,55 @@
+#include "Seven_Segment.h"
+
+uint32_t numbers[10] = {0};
+
+/**
+ * @brief This function is to be used to configure ports and pins seven segment
+ * @param[in] seven_segment The seven_segment data structure containing the port and pins to be configured
+ */
+void init_seven_segment(St_7_segment const *seven_segment)
+{
+    GPIO_InitTypeDef gpio_seven_out = {0};
+    gpio_seven_out.Mode = GPIO_MODE_OUTPUT_PP;
+    gpio_seven_out.Speed = GPIO_SPEED_FREQ_10MHZ;
+    gpio_seven_out.Pin = seven_segment->Pins;
+    Init_GPIO(seven_segment->Port,&gpio_seven_out);  
+
+    uint32_t io_current = 0;
+    uint32_t io_position = 0;
+    uint32_t start_pin_index = 0;
+    for(uint16_t pos; pos <=  GPIO_PIN_NUMBER ; pos++)
+    {
+        io_position = 1 << pos;
+        io_current = (uint32_t)seven_segment->Pins & io_position;
+        if(io_position == io_current)
+        {
+            start_pin_index = io_position;
+            break;
+        }
+    }
+    numbers[0]= (io_position << 0)|(io_position << 1)|(io_position << 2)|(io_position << 3)|(io_position << 4)|(io_position << 5);
+    numbers[1]= (io_position << 1)|(io_position << 2);
+    numbers[2]= (io_position << 0)|(io_position << 1)|(io_position << 6)|(io_position << 4)|(io_position << 3);
+    numbers[3]= (io_position << 0)|(io_position << 1)|(io_position << 6)|(io_position << 2)|(io_position << 3);
+    numbers[4]= (io_position << 5)|(io_position << 6)|(io_position << 1)|(io_position << 2);
+    numbers[5]= (io_position << 0)|(io_position << 5)|(io_position << 6)|(io_position << 2)|(io_position << 3);
+    numbers[6]= (io_position << 0)|(io_position << 5)|(io_position << 6)|(io_position << 4)|(io_position << 3)|(io_position << 2);
+    numbers[7]= (io_position << 0)|(io_position << 1)|(io_position << 2);
+    numbers[8]= (io_position << 0)|(io_position << 1)|(io_position << 2)|(io_position << 3)|(io_position << 4)|(io_position << 5)|(io_position << 6);
+    numbers[9]= (io_position << 0)|(io_position << 1)|(io_position << 2)|(io_position << 3)|(io_position << 5)|(io_position << 6);
+
+
+}
+
+/**
+ * @brief To print numbers on seven segment
+ * @param[in] seven_segment The seven_segment data structure containing the port and pins to be configured
+ * @param index The number to be printed
+ */
+void Display_seven_segment(St_7_segment *seven_segment, uint8_t index)
+{
+    uint32_t temp = seven_segment->Port->GPIO_ODR;
+    temp &= ~(seven_segment->Pins);
+    temp |= numbers[index];
+    Set_GPIO_Value(seven_segment->Port,temp);
+}
---
+++ unit_8/lesson 4/codes/Stm32f10x_UART.c
@@ -0,0 +1,463 @@
+/**
+ * @file Stm32f10x_UART.c
+ * @author adem alnajjar (adem.marangoz95@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-08-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "Stm32f10x_UART.h"
+#include "Stm32f10xx_GPIO.h"
+#include "Stm32f10xx_RCC.h"
+#include "Stm32f10xx_EXTI.h"
+#include <stdio.h>
+
+
+extern void _delay_ms(uint32_t time);
+void _Config_GPIO_f(St_Uart_API *Inst);
+extern St_Uart_API uart1_config;
+extern St_Uart_API uart2_config;
+extern St_Uart_API uart3_config;
+extern uint16_t Rx_Buff[10];
+
+
+
+/**
+ * @brief       is used to Set baud rate uart
+ * @param[in]   Inst : data structure for UART API    
+ * @return      None
+ */
+uint8_t Set_BaudRate(St_Uart_API *Inst)
+{
+    if(!(IS_UART_ALL_INSTANCE(Inst->UARTx))){return 0;}
+    uint32_t Bus_Freq = 0;
+    if(Inst->UARTx == UART1){Bus_Freq = Get_APB2_Freq();}
+    else{Bus_Freq = Get_APB1_Freq();}
+
+    uint32_t Div_Mantissa = Bus_Freq/(uint32_t)16;
+    Div_Mantissa = Div_Mantissa/(Inst->baudrate);
+    Inst->UARTx->UART_BRR |= (Div_Mantissa << 4);
+    uint32_t Div_Fraction = Bus_Freq/(uint32_t)16;
+    Div_Fraction = ((uint32_t)100*Div_Fraction)/(Inst->baudrate);
+    Div_Fraction = (Div_Fraction - Div_Mantissa*100)*16;
+    Div_Fraction = Div_Fraction/(uint32_t)100;
+    Inst->UARTx->UART_BRR |= Div_Fraction;
+    
+    return 1;
+}
+
+/**
+ * @brief       is used to get baud rate uart
+ * @param[in]   uart : data structure for UART API  
+ * @return      uint16_t : Baud Rate value
+ */
+uint16_t Get_BaudRate(St_Uart_API *Inst)
+{
+    //TODO
+}
+
+/**
+ * @brief       Is Used to Config UART
+ * @param[in]   uart : data structure for UART API   
+ * @return      uint8_t  : 1 = Succesed
+ *                         0 = Failed
+ */
+uint8_t Init_Uart(St_Uart_API *Inst)
+{
+    // Check Instance UART 
+    if(!(IS_UART_ALL_INSTANCE(Inst->UARTx))){return 0;}
+    
+    //Initially giving base functions addresses values
+    if(Inst->P_Error_CallBack == NULL){Inst->P_Error_CallBack = Error_CallBack_UART;}
+    if(Inst->P_Parity_CallBack == NULL){Inst->P_Parity_CallBack = Parity_CallBack_UART;}
+    if(Inst->P_Tx_CallBack == NULL){Inst->P_Tx_CallBack = Tx_CallBack_UART;}
+    if(Inst->P_Tx_Com_CallBack == NULL){Inst->P_Tx_Com_CallBack = Tx_Comp_CallBack_UART;}
+    if(Inst->P_Rx_CallBack == NULL){Inst->P_Rx_CallBack = Rx_CallBack_UART;}
+    if(Inst->P_IDLE_CallBack == NULL){Inst->P_IDLE_CallBack = IDLE_CallBack_UART;}
+    if(Inst->P_Lin_BK_CallBack == NULL){Inst->P_Lin_BK_CallBack = Lin_BK_CallBack_UART;}
+    if(Inst->P_FlowCTR_CallBack == NULL){Inst->P_FlowCTR_CallBack = FlowCt_CallBack_UART;}
+
+    // Config Pins
+    _Config_GPIO_f(Inst);
+
+    // Enable Uart Clock
+    if(Inst->UARTx == UART1){__APB2ENR_USART1EN_En();}
+    else if (Inst->UARTx == UART2){__APB1ENR_USART3EN_EN();}
+    else if (Inst->UARTx == UART3){__APB1ENR_USART2EN_EN();}
+
+    // Set Uart BuadRate
+    Set_BaudRate(Inst);
+    // Set Transmitter Enable to sends IDLE line 
+    if(Inst->Mode == Enable_RX){Inst->UARTx->UART_CR1 |=  Inst->Mode;} // Enable Just Rx
+    else if(Inst->Mode == Enable_Tx){Inst->UARTx->UART_CR1 |= Inst->Mode;} // Enable Just Tx
+    else {Inst->UARTx->UART_CR1 |= Inst->Mode;} // Enable Rx and Tx
+    //PCE: Parity control enable
+    Inst->UARTx->UART_CR1 |= Inst->Parity;
+    //Enable/Disable Flow Control CTS/RTS
+    Inst->UARTx->UART_CR3 |= Inst->Hw_Flow_CTRL;
+    //Stop bit
+    Inst->UARTx->UART_CR2 |= Inst->Stop_bit;
+    // Word Lenght
+    Inst->UARTx->UART_CR1 |= Inst->Word_Len;
+    
+
+    // for Enable Interrupt
+    if(Inst->interrupt != 0)
+    {
+        if(Inst->UARTx == UART1){NVIC_EnableIRQ(USART1_IRQn);}
+        else if (Inst->UARTx == UART2){NVIC_EnableIRQ(USART2_IRQn);}
+        else if(Inst->UARTx == UART3){NVIC_EnableIRQ(USART3_IRQn);}
+
+        if((Inst->interrupt & En_CR3_Interrupt) == En_CR3_Interrupt)
+        {
+            Inst->UARTx->UART_CR3 |= (Inst->interrupt & UART_CR3_EIE);
+            Inst->UARTx->UART_CR3 |= (Inst->interrupt & UART_CR3_CTSIE);
+        }
+
+        if((Inst->interrupt & En_CR2_Interrupt) == En_CR2_Interrupt)
+        {
+            Inst->UARTx->UART_CR2 |= (Inst->interrupt & UART_CR2_LBDIE);
+        }
+
+        if((Inst->interrupt & En_CR1_Interrupt) == En_CR1_Interrupt)
+        {
+            Inst->UARTx->UART_CR1 |= (Inst->interrupt & UART_CR1_PEIE);
+            Inst->UARTx->UART_CR1 |= (Inst->interrupt & UART_CR1_TXEIE);
+            Inst->UARTx->UART_CR1 |= (Inst->interrupt & UART_CR1_TCIE);
+            Inst->UARTx->UART_CR1 |= (Inst->interrupt & UART_CR1_RXNEIE);
+        }
+
+
+    }
+
+    // Enable the UART by writing the UE bit in UART_CR1 register to 1
+    Inst->UARTx->UART_CR1 |= UART_CR1_UE;
+    
+    return 1;
+}
+
+/**
+ * @brief       This function is used to reset UART Registers
+ * @param[in]   Inst  : data structure for UART API  
+ * @return      uint8_t 1 : Successed
+ *                      0 : Failed
+ */
+uint8_t DeInit_Uart(St_Uart_API *Inst)
+{
+    if(Inst->UARTx == UART1)
+    {
+        __APB2RSTR_USART1RST();
+        NVIC_DisableIRQ(USART1_IRQn);
+        
+    }else if(Inst->UARTx == UART2)
+    {
+        __APB1RSTR_USART2RST();
+        NVIC_DisableIRQ(USART2_IRQn);
+    }else if(Inst->UARTx == UART3)
+    {
+        __APB1RSTR_USART3RST();
+        NVIC_DisableIRQ(USART3_IRQn);
+    }
+    return 1;
+}
+
+
+/**
+ * @brief       Used to Confige UART pins
+ * @param[in]   uart : data structure for UART API  
+ */
+void _Config_GPIO_f(St_Uart_API *Inst)
+{
+    GPIO_InitTypeDef gpio_config = {0};
+    if(Inst->UARTx == UART1)
+    {
+        // Rx Pin config
+        gpio_config.Mode = GPIO_MODE_AF_INPUT;
+        gpio_config.Pin = GPIO_PIN_10;
+        Init_GPIO(GPIOA,&gpio_config);
+        // TX Pin config
+        gpio_config.Mode = GPIO_MODE_AF_PP;
+        gpio_config.Speed = GPIO_SPEED_FREQ_2MHZ;
+        gpio_config.Pin = GPIO_PIN_9;
+        Init_GPIO(GPIOA,&gpio_config);
+
+    }else if(Inst->UARTx == UART2)
+    {
+        // Rx Pin config
+        gpio_config.Mode = GPIO_MODE_AF_INPUT;
+        gpio_config.Pin = GPIO_PIN_3;
+        Init_GPIO(GPIOA,&gpio_config);
+        // Tx Pin config
+        gpio_config.Mode = GPIO_MODE_AF_PP;
+        gpio_config.Pin = GPIO_PIN_2;
+        gpio_config.Speed = GPIO_SPEED_FREQ_2MHZ;
+        Init_GPIO(GPIOA,&gpio_config);
+
+    }else if(Inst->UARTx == UART3)
+    {
+        // Rx Pin config
+        gpio_config.Mode = GPIO_MODE_AF_INPUT;
+        gpio_config.Pin = GPIO_PIN_11;
+        Init_GPIO(GPIOB,&gpio_config);
+        // Tx Pin config
+        gpio_config.Mode = GPIO_MODE_AF_PP;
+        gpio_config.Pin = GPIO_PIN_10;
+        gpio_config.Speed = GPIO_SPEED_FREQ_2MHZ;
+        Init_GPIO(GPIOB,&gpio_config);
+
+    }
+}
+
+/**
+ * @brief   This function is used to send a single character through uart 
+ * @param   Inst : UART Data Structure ,It can be replaced by UART1 or UART2 or UART3
+ * @param   msg  : First address of sending massage
+ * @return  uint8_t 1 : Successed
+ *                  0 : Failed
+ */
+uint8_t Send_Char_Uart(St_UART_Typedef *Inst,uint16_t *msg, enum Polling_mechism PollingEn)
+{
+    // Cgeck TXE bit  : if set meaning the data is transferred to shift register
+    if(PollingEn == Enable)
+        while(!(Inst->UART_SR & UART_SR_TXE));
+    
+    if((Inst->UART_CR1 & UART_CR1_PCE) == UART_CR1_PCE)
+    {
+        Inst->UART_DR = (*msg & (uint16_t)0x01FF);
+    }else
+    {
+        Inst->UART_DR =(*msg & (uint8_t)0xFF);
+    }
+    return 1;
+}
+
+/**
+ * @brief   This function is used to receive a singal character through uart
+ * @param   Inst : UART Data Structure ,It can be replaced by UART1 or UART2 or UART3
+ * @param   msg  : First address of receiving massage
+ * @return  uint8_t 1 : Successed
+ *                  0 : Failed 
+ */
+uint8_t Receive_Char_Uart(St_UART_Typedef *Inst, uint16_t *msg, enum Polling_mechism PollingEn)
+{
+    // Check RXNE bit : if set meaning the data is transferred to shift register 
+    if(PollingEn == Enable)
+        while(!(Inst->UART_SR & UART_SR_RXNE));
+
+    if((Inst->UART_CR1 & UART_CR1_M) == UART_CR1_M)
+    {
+        if((Inst->UART_CR1 & UART_CR1_PCE) == UART_CR1_PCE)
+        {
+            *((uint16_t*)msg) = (Inst->UART_DR & (uint8_t)0xFF);
+        }else
+        {
+            *((uint16_t*)msg) = Inst->UART_DR;
+        }
+    }else
+    {
+        if((Inst->UART_CR1 & UART_CR1_PCE) == UART_CR1_PCE)
+        {
+            *((uint16_t*)msg) = (Inst->UART_DR & (uint8_t)0x7F);
+        }else
+        {
+            *((uint16_t*)msg) = (Inst->UART_DR & (uint8_t)0xFF);
+        }
+    }
+    
+    return 1;
+}
+
+/**
+ * @brief   This function used to Receiv string through uart
+ * @param   Inst : UART Data Structure ,It can be replaced by UART1 or UART2 or UART3
+ * @param   msg  : First address of receiving massage
+ * @param   msg_len : Massege length 
+ * @return  uint8_t 1 : Successed
+ *                  0 : Failed  
+ */
+uint8_t Receive_String_Uart(St_UART_Typedef *Inst, uint16_t *msg, uint8_t msg_len, enum Polling_mechism PollingEn)
+{
+    while(*msg != '\0')
+    {
+        
+    }
+}
+
+/**
+ * @brief   This function used to Send string through uart
+ * @param   Inst : UART Data Structure ,It can be replaced by UART1 or UART2 or UART3
+ * @param   msg  : First address of receiving massage
+ * @param   msg_len : Massege length 
+ * @return  uint8_t 1 : Successed
+ *                  0 : Failed  
+ */
+uint8_t Send_String_Uart(St_UART_Typedef *Inst, uint16_t *msg, enum Polling_mechism PollingEn)
+{
+    while (*msg != '\0')
+    {
+        Send_Char_Uart(Inst,msg,PollingEn);
+        msg++;
+        _delay_ms(10);
+    }
+    
+}
+
+/**
+ * @brief This function handles USART1 global interrupt.
+ */
+void USART1_Handler(void)
+{
+    if((UART1->UART_CR3 & UART_CR3_EIE) == UART_CR3_EIE)
+        uart1_config.P_Error_CallBack(&uart1_config);   //  jump to defualt Error_CallBack_UART function
+    else if((UART1->UART_CR1 & UART_CR1_PEIE) == UART_CR1_PEIE)
+        uart1_config.P_Parity_CallBack(&uart1_config);  //  jump to defualt Parity_CallBack_UART function
+    else if((UART1->UART_CR1 & UART_CR1_TXEIE) == UART_CR1_TXEIE)
+        uart1_config.P_Tx_CallBack(&uart1_config);      //  jump to defualt Tx_CallBack_UART function
+    else if((UART1->UART_CR1 & UART_CR1_TCIE) == UART_CR1_TCIE)
+        uart1_config.P_Tx_Com_CallBack(&uart1_config);  //  jump to defualt Tx_Comp_CallBack_UART function
+    else if((UART1->UART_CR1 & UART_CR1_RXNEIE) == UART_CR1_RXNEIE)
+        uart1_config.P_Rx_CallBack(&uart1_config);      //  jump to defualt Rx_CallBack_UART function
+    else if((UART1->UART_CR1 & UART_CR1_IDLEIE) == UART_CR1_IDLEIE)
+        uart1_config.P_IDLE_CallBack(&uart1_config);    //  jump to defualt IDLE_CallBack_UART function
+    else if((UART1->UART_CR2 & UART_CR2_LBDIE) == UART_CR2_LBDIE)
+        uart1_config.P_Lin_BK_CallBack(&uart1_config);  //  jump to defualt Lin_BK_CallBack_UART function
+    else if((UART1->UART_CR3 & UART_CR3_CTSIE) == UART_CR3_CTSIE)
+        uart1_config.P_FlowCTR_CallBack(&uart1_config); //  jump to defualt FlowCt_CallBack_UART function
+
+}
+
+void USART2_Handler(void)
+{
+    // if((UART2->UART_CR3 & UART_CR3_EIE) == UART_CR3_EIE)
+    //     uart2_config.P_Error_CallBack(&uart2_config);
+    // else if((UART2->UART_CR1 & UART_CR1_PEIE) == UART_CR1_PEIE)
+    //     uart2_config.P_Parity_CallBack(&uart2_config);
+    // else if((UART2->UART_CR1 & UART_CR1_TXEIE) == UART_CR1_TXEIE)
+    //     uart2_config.P_Tx_CallBack(&uart2_config);
+    // else if((UART2->UART_CR1 & UART_CR1_TCIE) == UART_CR1_TCIE)
+    //     uart2_config.P_Tx_Com_CallBack(&uart2_config);
+    // else if((UART2->UART_CR1 & UART_CR1_RXNEIE) == UART_CR1_RXNEIE)
+    //     uart2_config.P_Rx_CallBack(&uart2_config);
+    // else if((UART2->UART_CR1 & UART_CR1_IDLEIE) == UART_CR1_IDLEIE)
+    //     uart2_config.P_IDLE_CallBack(&uart2_config);
+    // else if((UART2->UART_CR2 & UART_CR2_LBDIE) == UART_CR2_LBDIE)
+    //     uart2_config.P_Lin_BK_CallBack(&uart2_config);
+    // else if((UART2->UART_CR3 & UART_CR3_CTSIE) == UART_CR3_CTSIE)
+    //     uart2_config.P_FlowCTR_CallBack(&uart2_config);
+
+}
+
+void USART3_Handler(void)
+{
+    // if((UART3->UART_CR3 & UART_CR3_EIE) == UART_CR3_EIE)
+    //     uart3_config.P_Error_CallBack(&uart3_config);
+    // else if((UART3->UART_CR1 & UART_CR1_PEIE) == UART_CR1_PEIE)
+    //     uart3_config.P_Parity_CallBack(&uart3_config);
+    // else if((UART3->UART_CR1 & UART_CR1_TXEIE) == UART_CR1_TXEIE)
+    //     uart3_config.P_Tx_CallBack(&uart3_config);
+    // else if((UART3->UART_CR1 & UART_CR1_TCIE) == UART_CR1_TCIE)
+    //     uart3_config.P_Tx_Com_CallBack(&uart3_config);
+    // else if((UART3->UART_CR1 & UART_CR1_RXNEIE) == UART_CR1_RXNEIE)
+    //     uart3_config.P_Rx_CallBack(&uart3_config);
+    // else if((UART3->UART_CR1 & UART_CR1_IDLEIE) == UART_CR1_IDLEIE)
+    //     uart3_config.P_IDLE_CallBack(&uart3_config);
+    // else if((UART3->UART_CR2 & UART_CR2_LBDIE) == UART_CR2_LBDIE)
+    //     uart3_config.P_Lin_BK_CallBack(&uart3_config);
+    // else if((UART3->UART_CR3 & UART_CR3_CTSIE) == UART_CR3_CTSIE)
+    //     uart3_config.P_FlowCTR_CallBack(&uart3_config);
+
+}
+
+
+/**
+ * @brief Parity Error Interrupt defualt CallBacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module
+ * @retval None
+ */
+__weak void Parity_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief Error Flag Interrupt defualt CallBacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module
+ * @return None
+ */
+__weak void Error_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief Tx Interrupt defualt CallBacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module 
+ * @return None 
+ */
+__weak void Tx_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief Tx Complate Interrupt defualt CallBacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module
+ * @return None
+ */
+__weak void Tx_Comp_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief Rx Interrupt defualt CallBacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module 
+ * @return None 
+ */
+__weak void Rx_CallBack_UART(St_Uart_API *UARTx)
+{
+    Receive_Char_Uart(UART1, Rx_Buff,Disable);
+    Send_Char_Uart(UART1, Rx_Buff,Enable);
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief IDLE interrupt defualt CallBacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module
+ * @return None
+ */
+__weak void IDLE_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief LIN Break Interrupt defualt Callbacks
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module
+ * @return None
+ */
+__weak void Lin_BK_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
+
+/**
+ * @brief CTS/RTS Interrupts defualt CallBacks 
+ * @param UARTx  Pointer to a St_Uart_API structure that contains
+ *               the configuration information for the specified UART module
+ * @return None
+ */
+__weak void FlowCt_CallBack_UART(St_Uart_API *UARTx)
+{
+    UNUSED(UARTx);
+}
