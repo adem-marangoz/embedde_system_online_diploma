754ad89e2c7a8e3b6effeed61d84784373c275a8
adem-marangoz <78273512+adem-marangoz@users.noreply.github.com>
2022-09-23 03:17:00 -0700

    new commant

--- unit_9/lesson 1/WTDG/Atmega32_test1/lib/Src/WTDG.c
+++ unit_9/lesson 1/WTDG/Atmega32_test1/lib/Src/WTDG.c
@@ -1,7 +1,7 @@
 /**
  * @file WTDG.c
  * @author adem alnajjar (adem.marangoz@gmail.com)
- * @brief 
+ * @brief This File is Used to control for Watachdog 
  * @version 0.1
  * @date 2022-09-21
  * 
@@ -19,6 +19,9 @@
 
 //________________________________ GLOBAL FUNCTIONS ____________________________
 
+/**
+* @brief This function is used to Init Watach dog
+*/
 void WTDG_Init(En_WTDG_Pre Pre)
 {
     WDTCR = 00;
@@ -26,12 +29,18 @@ void WTDG_Init(En_WTDG_Pre Pre)
 }
 
 
+/**
+* @brief This Function is used to starts watach dog counting
+*/
 void WTDG_Start()
 {
     WDTCR |= (1 << WDE);
 }
 
 
+/**
+* @brief This function is used to Stops watach dog counting and resets
+*/
 void WTDG_Stop()
 {
     WDTCR |= (1 << WDE) | (1 << WDTOE);
